478804b2b65bf6809b39c1ff9c4d4f56029273c0
Ryan French <French.Ryan.K@gmail.com>
2011-05-11 21:51:25 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IVC::Application.config.secret_token = '29389a5ac898b0cf6010ad7a6f33d93bb91c5037b2117ecb33288e5c02b677de9507f5d1bdc2bd21237a866901416b32575d933c10255d6a1230f861e83b71d8'
